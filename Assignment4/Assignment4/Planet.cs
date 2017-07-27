5d81b677abeb032199dd418ba863023cb3c309d4
FuadMannan <fuad.mannan@gmail.com>
2017-07-27 02:01:29 -0400

    Added basic members of Planet Class

--- Assignment4/Assignment4/Planet.cs
+++ Assignment4/Assignment4/Planet.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 /* Name: Fuad Mannan
  * Date: July 27, 2017
  * Description: This is the Abstract Planet class
- * Version: 0.1 - Class created
+ * Version: 0.2 - Added private instance variables, properties, constructor, ToString
  */
 
 namespace Assignment4
@@ -17,6 +17,32 @@ namespace Assignment4
     /// </summary>
     public abstract class Planet
     {
+        // Private Instance Variables
+        private double _diameter, _mass, _orbitalPeriod, _rotationalPeriod;
+        private int _moonCount, _ringCount;
+        private string _name;
 
+        // Public Properties
+        public double Diameter { get; }
+        public double Mass { get; }
+        public int MoonCount { get; set; }
+        public string Name { get; }
+        public double OrbitalPeriod { get; set; }
+        public int RingCount { get; set; }
+        public double MyProperty { get; set; }
+
+        // Constructor
+        public Planet(string name,double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
+
+        // ToString() Function
+        public override string ToString()
+        {
+            return string.Format("Planet: {0}, Diameter: {1}, Mass: {2}", Name, Diameter, Mass);
+        }
     }
 }
