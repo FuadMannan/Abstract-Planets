d1b1492f49007ffe3c605148e444b15059f2e734
FuadMannan <fuad.mannan@gmail.com>
2017-07-27 11:13:32 -0400

    Added objects and WaitForAnyKey method to main

--- Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 /* Name: Fuad Mannan
  * Date: July 27, 2017
  * Description: This is the "Driver" class
- * Version: 0.2 - Class updated
+ * Version: 0.3 - Added planet objects to Main, created WaitForAnyKey method
  */
 
 namespace Assignment4
@@ -16,7 +16,15 @@ namespace Assignment4
     {
         static void Main(string[] args)
         {
-            GiantPlanet Saturn = new GiantPlanet("Saturn", 2000, 2000,"gas");
+            GiantPlanet giantPlanetObj = new GiantPlanet("Saturn", 2000, 2000,"Gas");
+            Console.WriteLine("First Giant Planet: {0}",giantPlanetObj.ToString());
+            TerrestrialPlanet terrestrialPlanetObj = new TerrestrialPlanet("Earth", 200, 200, true);
+            Console.WriteLine("First Terrestrial Planet: {0}",terrestrialPlanetObj.ToString());
+            WaitForAnyKey();
+        }
+        static void WaitForAnyKey()
+        {
+            Console.ReadKey();
         }
     }
 }
