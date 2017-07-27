ec534b5343a5c68ae628baed5a88865a678a683c
FuadMannan <fuad.mannan@gmail.com>
2017-07-27 03:37:11 -0400

    Updated driver class

--- Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 /* Name: Fuad Mannan
  * Date: July 27, 2017
  * Description: This is the "Driver" class
- * Version: 0.1 - Class created
+ * Version: 0.2 - Class updated
  */
 
 namespace Assignment4
@@ -16,6 +16,7 @@ namespace Assignment4
     {
         static void Main(string[] args)
         {
+            GiantPlanet Saturn = new GiantPlanet("Saturn", 2000, 2000,"gas");
         }
     }
 }
