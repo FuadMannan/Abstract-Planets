3e800e76428a2458c08bdbc6e045e94d97ae898c
FuadMannan <fuad.mannan@gmail.com>
2017-07-27 01:21:50 -0400

    Created main program and Planet class files.

---
+++ Assignment4/Assignment4/Planet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name: Fuad Mannan
+ * Date: July 27, 2017
+ * Description: This is the Abstract Planet class
+ * Version: 0.1 - Class created
+ */
+
+namespace Assignment4
+{
+    /// <summary>
+    /// This is the Abstract Planet Class
+    /// </summary>
+    public abstract class Planet
+    {
+
+    }
+}
---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name: Fuad Mannan
+ * Date: July 27, 2017
+ * Description: This is the "Driver" class
+ * Version: 0.1 - Class created
+ */
+
+namespace Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
